b08ef5608c5110e083c60a055de7e40256dddedd
Gustavo Krug <gustavo.krug@hotmail.com>
2018-06-04 21:38:32 -0400

    SQL corregido

--- trunk/doctrack/sql/scripts2.sql
+++ trunk/doctrack/sql/scripts2.sql
@@ -56,7 +56,6 @@ CREATE TABLE `tramitantes` (
 
 INSERT INTO `tramitantes` VALUES (1,'Juan Perez','su casa','000','0005','juan@gmail.com'),(2,'nuevo tramitantes','su casa','552','545','suemail@gmail.com');
 
-
 DROP TABLE IF EXISTS `superviciones`;
 CREATE TABLE `superviciones` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -75,62 +74,6 @@ CREATE TABLE `superviciones` (
 
 INSERT INTO `superviciones` VALUES (1,'5','carlos antonio lopez','5552','suemail@gmail.com',1,1);
 
-
-DROP TABLE IF EXISTS `documentos`;
-CREATE TABLE `documentos` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(50) NOT NULL DEFAULT '',
-  `fechaentrada` datetime NOT NULL,
-  `observacion` varchar(255) NOT NULL DEFAULT '',
-  `idSupervicion` bigint(20) NOT NULL,
-  `idTramitante` bigint(20) NOT NULL,
-  `numero_doc` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  KEY `fk_idSupervicion` (`idSupervicion`),
-  KEY `fk_idTramitante` (`idTramitante`),
-  CONSTRAINT `fk_idSupervicion` FOREIGN KEY (`idSupervicion`) REFERENCES `superviciones` (`id`),
-  CONSTRAINT `fk_idTramitante` FOREIGN KEY (`idTramitante`) REFERENCES `tramitantes` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `documentos` VALUES (1,'documento prueba','2018-05-29 01:30:00','sin obs',1,1,'00025/18');
-
-
-DROP TABLE IF EXISTS `seguimiento`;
-CREATE TABLE `seguimiento` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `fechaentrada` datetime NOT NULL,
-  `fechasalido` datetime NOT NULL,
-  `estadogeneral` varchar(50) NOT NULL DEFAULT '',
-  `descripcion` varchar(255) NOT NULL DEFAULT '',
-  `idDocumento` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idDocumento` (`idDocumento`),
-  CONSTRAINT `seguimiento_ibfk_1` FOREIGN KEY (`idDocumento`) REFERENCES `documentos` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-DROP TABLE IF EXISTS `gestiondocumentos`;
-CREATE TABLE `gestiondocumentos` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `fecha` datetime NOT NULL,
-  `asunto` varchar(255) NOT NULL DEFAULT '',
-  `observacion` varchar(255) NOT NULL DEFAULT '',
-  `estadogestion` varchar(255) NOT NULL DEFAULT '',
-  `idDependencia` bigint(20) NOT NULL,
-  `idArea` bigint(20) NOT NULL,
-  `idSeguimiento` bigint(20) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `idDependencia` (`idDependencia`),
-  KEY `idArea` (`idArea`),
-  KEY `idSeguimiento` (`idSeguimiento`),
-  CONSTRAINT `gestiondocumentos_ibfk_3` FOREIGN KEY (`idSeguimiento`) REFERENCES `seguimiento` (`id`),
-  CONSTRAINT `gestiondocumentos_ibfk_1` FOREIGN KEY (`idDependencia`) REFERENCES `dependencias` (`id`),
-  CONSTRAINT `gestiondocumentos_ibfk_2` FOREIGN KEY (`idArea`) REFERENCES `areas` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
 DROP TABLE IF EXISTS `tipodocumento`;
 CREATE TABLE `tipodocumento` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -139,40 +82,6 @@ CREATE TABLE `tipodocumento` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 INSERT INTO `tipodocumento` VALUES (1,'nota ac'),(2,'nueva nota');
-
-
-
-DROP TABLE IF EXISTS `adjuntos`;
-CREATE TABLE `adjuntos` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `nombredoc_adj` varchar(255) NOT NULL DEFAULT '',
-  `idTipodoc` bigint(20) NOT NULL,
-  `idDocumento` bigint(20) NOT NULL,
-  KEY `idTipodoc` (`idTipodoc`),
-  KEY `idDocumento` (`idDocumento`),
-  PRIMARY KEY (`id`),
-  CONSTRAINT `adjuntos_ibfk_1` FOREIGN KEY (`idTipodoc`) REFERENCES `tipodocumento` (`id`),
-  CONSTRAINT `adjuntos_ibfk_2` FOREIGN KEY (`idDocumento`) REFERENCES `documentos` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-
-DROP TABLE IF EXISTS `resolucion`;
-CREATE TABLE `resolucion` (
-  `id` bigint(20) NOT NULL,
-  `fecha` datetime NOT NULL,
-  `numero` varchar(50) NOT NULL DEFAULT '',
-  `resuelve` varchar(255) NOT NULL DEFAULT '',
-  `visto` varchar(255) NOT NULL DEFAULT '',
-  `idFuncionario` bigint(20) NOT NULL,
-  `idSeguimiento` bigint(20) NOT NULL,
-  UNIQUE KEY `id` (`id`),
-  KEY `idFuncionario` (`idFuncionario`),
-  KEY `idSeguimiento` (`idSeguimiento`),
-  CONSTRAINT `resolucion_ibfk_1` FOREIGN KEY (`idFuncionario`) REFERENCES `funcionarios` (`id`),
-  CONSTRAINT `resolucion_ibfk_2` FOREIGN KEY (`idSeguimiento`) REFERENCES `seguimiento` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `usuarios`;
 DROP TABLE IF EXISTS `roles`;
@@ -211,26 +120,6 @@ CREATE TABLE `logs` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8225 DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `respuesta`;
-CREATE TABLE `respuesta` (
-  `id` bigint(20) NOT NULL,
-  `fecha` datetime NOT NULL,
-  `numero` varchar(50) NOT NULL DEFAULT '',
-  `dirigidoa` varchar(255) NOT NULL DEFAULT '',
-  `remitente` varchar(255) NOT NULL DEFAULT '',
-  `asunto` varchar(255) NOT NULL DEFAULT '',
-  `redaccion` varchar(255) NOT NULL DEFAULT '',
-  `idFuncionario` bigint(20) NOT NULL,
-  `idSeguimiento` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idFuncionario` (`idFuncionario`),
-  KEY `idSeguimiento` (`idSeguimiento`),
-  KEY `idTipoDoc` (`idTipoDoc`),
-  CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`idFuncionario`) REFERENCES `funcionarios` (`id`),
-  CONSTRAINT `respuesta_ibfk_2` FOREIGN KEY (`idSeguimiento`) REFERENCES `seguimiento` (`id`),
-  CONSTRAINT `respuesta_ibfk_3` FOREIGN KEY (`idTipoDoc`) REFERENCES `tipodocumento` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 DROP TABLE IF EXISTS `documentos`;
 CREATE TABLE `documentos` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -248,6 +137,76 @@ CREATE TABLE `documentos` (
   CONSTRAINT `fk_idSupervicion` FOREIGN KEY (`idSupervicion`) REFERENCES `superviciones` (`id`),
   CONSTRAINT `fk_idTipodoc` FOREIGN KEY (`idTipodoc`) REFERENCES `tipodocumento` (`id`),
   CONSTRAINT `fk_idTramitante` FOREIGN KEY (`idTramitante`) REFERENCES `tramitantes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `seguimiento`;
+CREATE TABLE `seguimiento` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `fechaentrada` datetime NOT NULL,
+  `fechasalido` datetime NOT NULL,
+  `estadogeneral` varchar(50) NOT NULL DEFAULT '',
+  `descripcion` varchar(255) NOT NULL DEFAULT '',
+  `idDocumento` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idDocumento` (`idDocumento`),
+  CONSTRAINT `seguimiento_ibfk_1` FOREIGN KEY (`idDocumento`) REFERENCES `documentos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `respuesta`;
+CREATE TABLE `respuesta` (
+  `id` bigint(20) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `numero` varchar(50) NOT NULL DEFAULT '',
+  `dirigidoa` varchar(255) NOT NULL DEFAULT '',
+  `remitente` varchar(255) NOT NULL DEFAULT '',
+  `asunto` varchar(255) NOT NULL DEFAULT '',
+  `redaccion` varchar(255) NOT NULL DEFAULT '',
+  `idFuncionario` bigint(20) NOT NULL,
+  `idSeguimiento` bigint(20) NOT NULL,
+  `idTipoDoc` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idFuncionario` (`idFuncionario`),
+  KEY `idSeguimiento` (`idSeguimiento`),
+  KEY `idTipoDoc` (`idTipoDoc`),
+  CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`idFuncionario`) REFERENCES `funcionarios` (`id`),
+  CONSTRAINT `respuesta_ibfk_2` FOREIGN KEY (`idSeguimiento`) REFERENCES `seguimiento` (`id`),
+  CONSTRAINT `respuesta_ibfk_3` FOREIGN KEY (`idTipoDoc`) REFERENCES `tipodocumento` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `gestiondocumentos`;
+CREATE TABLE `gestiondocumentos` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `fecha` datetime NOT NULL,
+  `asunto` varchar(255) NOT NULL DEFAULT '',
+  `observacion` varchar(255) NOT NULL DEFAULT '',
+  `estadogestion` varchar(255) NOT NULL DEFAULT '',
+  `idDependencia` bigint(20) NOT NULL,
+  `idArea` bigint(20) NOT NULL,
+  `idSeguimiento` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idDependencia` (`idDependencia`),
+  KEY `idArea` (`idArea`),
+  KEY `idSeguimiento` (`idSeguimiento`),
+  CONSTRAINT `gestiondocumentos_ibfk_3` FOREIGN KEY (`idSeguimiento`) REFERENCES `seguimiento` (`id`),
+  CONSTRAINT `gestiondocumentos_ibfk_1` FOREIGN KEY (`idDependencia`) REFERENCES `dependencias` (`id`),
+  CONSTRAINT `gestiondocumentos_ibfk_2` FOREIGN KEY (`idArea`) REFERENCES `areas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `resolucion`;
+CREATE TABLE `resolucion` (
+  `id` bigint(20) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `numero` varchar(50) NOT NULL DEFAULT '',
+  `resuelve` varchar(255) NOT NULL DEFAULT '',
+  `visto` varchar(255) NOT NULL DEFAULT '',
+  `idFuncionario` bigint(20) NOT NULL,
+  `idSeguimiento` bigint(20) NOT NULL,
+  UNIQUE KEY `id` (`id`),
+  KEY `idFuncionario` (`idFuncionario`),
+  KEY `idSeguimiento` (`idSeguimiento`),
+  CONSTRAINT `resolucion_ibfk_1` FOREIGN KEY (`idFuncionario`) REFERENCES `funcionarios` (`id`),
+  CONSTRAINT `resolucion_ibfk_2` FOREIGN KEY (`idSeguimiento`) REFERENCES `seguimiento` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO roles (descripcion, administra_configuraciones)
